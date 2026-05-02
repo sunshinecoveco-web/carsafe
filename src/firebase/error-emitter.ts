8161c9593eca65756200786afacd9fadee0bf1e2
sunshinecoveco-web <sunshinecove.co@gmail.com>
2026-05-02 21:37:29 +0200

    Create error-emitter.ts

---
+++ src/firebase/error-emitter.ts
@@ -0,0 +1,39 @@
+// A simple typed event emitter
+type EventMap = Record<string, any>;
+type EventKey<T extends EventMap> = string & keyof T;
+type EventReceiver<T> = (params: T) => void;
+
+interface Emitter<T extends EventMap> {
+  on<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
+  off<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
+  emit<K extends EventKey<T>>(eventName: K, params: T[K]): void;
+}
+
+export function createEmitter<T extends EventMap>(): Emitter<T> {
+  const listeners: { [K in keyof T]?: Array<(p: T[K]) => void> } = {};
+
+  return {
+    on(eventName, fn) {
+      listeners[eventName] = (listeners[eventName] || []).concat(fn);
+    },
+    off(eventName, fn) {
+      listeners[eventName] = (listeners[eventName] || []).filter(
+        (f) => f !== fn
+      );
+    },
+    emit(eventName, params) {
+      (listeners[eventName] || []).forEach(function (fn) {
+        fn(params);
+      });
+    },
+  };
+}
+
+// Specific for our app's errors
+import type { FirestorePermissionError } from './errors';
+
+interface ErrorEvents {
+    'permission-error': FirestorePermissionError;
+}
+
+export const errorEmitter = createEmitter<ErrorEvents>();
