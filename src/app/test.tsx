d573673a7ffab0751bbd3b4977fd0364ffd66f12
sunshinecoveco-web <sunshinecove.co@gmail.com>
2026-05-10 22:09:22 +0200

    Create test.tsx

---
+++ src/app/test.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 50, fontSize: 24, color: 'black', background: 'white' }}>
+      <h1>CarSafe Test Page 🚗</h1>
+      <p>If you see this, basic rendering works!</p>
+    </div>
+  );
+}
