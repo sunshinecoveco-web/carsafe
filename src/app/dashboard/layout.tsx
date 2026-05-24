c2a840fe4fa16d1a9ceaac2d13725c43285de2b1
sunshinecoveco-web <sunshinecove.co@gmail.com>
2026-05-24 20:00:31 +0000

    Fix duplicate header

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -70,7 +70,7 @@ export default function DashboardLayout({
 
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <Header />
+      
       <main className="flex-1">{children}</main>
     </div>
   );
